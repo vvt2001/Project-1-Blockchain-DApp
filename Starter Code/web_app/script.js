a8b4c41c99445417407724ecfbd65aaaced270ae
Vu Xuan Bac <vuxuanbac712@gmail.com>
2023-07-14 16:08:15 +0700

    Bug fix

--- Starter Code/web_app/script.js
+++ Starter Code/web_app/script.js
@@ -14,148 +14,148 @@ var abi = [{
     "stateMutability": "nonpayable",
     "type": "constructor"
 },
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "iAddress",
-                "type": "address"
-            },
-            {
-                "internalType": "address",
-                "name": "uAddress",
-                "type": "address"
-            },
-            {
-                "internalType": "uint32",
-                "name": "amount",
-                "type": "uint32"
-            }
-        ],
-        "name": "add_IOU",
-        "outputs": [],
-        "stateMutability": "nonpayable",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "input",
-                "type": "address"
-            }
-        ],
-        "name": "getAllOwingData",
-        "outputs": [
-            {
-                "components": [
-                    {
-                        "internalType": "uint32",
-                        "name": "amount",
-                        "type": "uint32"
-                    },
-                    {
-                        "internalType": "address",
-                        "name": "owingAddress",
-                        "type": "address"
-                    }
-                ],
-                "internalType": "struct Splitwise.Owing[]",
-                "name": "",
-                "type": "tuple[]"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "getParticipant",
-        "outputs": [
-            {
-                "internalType": "address[]",
-                "name": "",
-                "type": "address[]"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "debtor",
-                "type": "address"
-            },
-            {
-                "internalType": "address",
-                "name": "creditor",
-                "type": "address"
-            }
-        ],
-        "name": "lookup",
-        "outputs": [
-            {
-                "internalType": "uint32",
-                "name": "ret",
-                "type": "uint32"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [],
-        "name": "owner",
-        "outputs": [
-            {
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-            }
-        ],
-        "name": "participant",
-        "outputs": [
-            {
-                "internalType": "bool",
-                "name": "",
-                "type": "bool"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    },
-    {
-        "inputs": [
-            {
-                "internalType": "uint256",
-                "name": "",
-                "type": "uint256"
-            }
-        ],
-        "name": "participantList",
-        "outputs": [
-            {
-                "internalType": "address",
-                "name": "",
-                "type": "address"
-            }
-        ],
-        "stateMutability": "view",
-        "type": "function"
-    }]; // FIXME: fill this in with your contract's ABI //Be sure to only have one array, not two
+{
+    "inputs": [
+        {
+            "internalType": "address",
+            "name": "iAddress",
+            "type": "address"
+        },
+        {
+            "internalType": "address",
+            "name": "uAddress",
+            "type": "address"
+        },
+        {
+            "internalType": "uint32",
+            "name": "amount",
+            "type": "uint32"
+        }
+    ],
+    "name": "add_IOU",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+},
+{
+    "inputs": [
+        {
+            "internalType": "address",
+            "name": "input",
+            "type": "address"
+        }
+    ],
+    "name": "getAllOwingData",
+    "outputs": [
+        {
+            "components": [
+                {
+                    "internalType": "uint32",
+                    "name": "amount",
+                    "type": "uint32"
+                },
+                {
+                    "internalType": "address",
+                    "name": "owingAddress",
+                    "type": "address"
+                }
+            ],
+            "internalType": "struct Splitwise.Owing[]",
+            "name": "",
+            "type": "tuple[]"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+},
+{
+    "inputs": [],
+    "name": "getParticipant",
+    "outputs": [
+        {
+            "internalType": "address[]",
+            "name": "",
+            "type": "address[]"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+},
+{
+    "inputs": [
+        {
+            "internalType": "address",
+            "name": "debtor",
+            "type": "address"
+        },
+        {
+            "internalType": "address",
+            "name": "creditor",
+            "type": "address"
+        }
+    ],
+    "name": "lookup",
+    "outputs": [
+        {
+            "internalType": "uint32",
+            "name": "ret",
+            "type": "uint32"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+},
+{
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+        {
+            "internalType": "address",
+            "name": "",
+            "type": "address"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+},
+{
+    "inputs": [
+        {
+            "internalType": "address",
+            "name": "",
+            "type": "address"
+        }
+    ],
+    "name": "participant",
+    "outputs": [
+        {
+            "internalType": "bool",
+            "name": "",
+            "type": "bool"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+},
+{
+    "inputs": [
+        {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+        }
+    ],
+    "name": "participantList",
+    "outputs": [
+        {
+            "internalType": "address",
+            "name": "",
+            "type": "address"
+        }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+}]; // FIXME: fill this in with your contract's ABI //Be sure to only have one array, not two
 // ============================================================
 abiDecoder.addABI(abi);
 // call abiDecoder.decodeMethod to use this - see 'getAllFunctionCalls' for more
@@ -199,8 +199,8 @@ async function findTheLeastAmount(path) {
             );
             if (result[j].owingAddress == path[i]) {
                 //auto put the first node's weight to track
-                if (i == 1 || result[j].amount.toNumber() < amount) {
-                    amount = result[j].amount.toNumber();
+                if (i == 1 || Number(result[j].amount) < amount) {
+                    amount = Number(result[j].amount);
                 }
             }
         }
@@ -262,7 +262,7 @@ async function add_IOU(creditor, amount) {
 
     // Do the BFS to find the cycle.
     const cyclePath = await doBFS(
-        defaultAccount,
+        creditor,
         defaultAccount,
         getNeighbors
     );
@@ -287,6 +287,7 @@ async function add_IOU(creditor, amount) {
         );
         await BlockchainSplitwise.add_IOU(cyclePath[i], cyclePath[i - 1], leastAmount);
     }
+    await BlockchainSplitwise.add_IOU(cyclePath[0], cyclePath[cyclePath.length - 1], leastAmount);
 }
 
 
@@ -298,55 +299,55 @@ async function add_IOU(creditor, amount) {
 // This searches the block history for all calls to 'functionName' (string) on the 'addressOfContract' (string) contract
 // It returns an array of objects, one for each call, containing the sender ('from'), arguments ('args'), and the timestamp ('t')
 async function getAllFunctionCalls(addressOfContract, functionName) {
-	var curBlock = await provider.getBlockNumber();
-	var function_calls = [];
+    var curBlock = await provider.getBlockNumber();
+    var function_calls = [];
 
-	while (curBlock !== GENESIS) {
-	  var b = await provider.getBlockWithTransactions(curBlock);
-	  var txns = b.transactions;
-	  for (var j = 0; j < txns.length; j++) {
-	  	var txn = txns[j];
+    while (curBlock !== GENESIS) {
+        var b = await provider.getBlockWithTransactions(curBlock);
+        var txns = b.transactions;
+        for (var j = 0; j < txns.length; j++) {
+            var txn = txns[j];
 
-	  	// check that destination of txn is our contract
-			if(txn.to == null){continue;}
-	  	if (txn.to === addressOfContract) {
-	  		var func_call = abiDecoder.decodeMethod(txn.data);
+            // check that destination of txn is our contract
+            if (txn.to == null) { continue; }
+            if (txn.to === addressOfContract) {
+                var func_call = abiDecoder.decodeMethod(txn.data);
 
-				// check that the function getting called in this txn is 'functionName'
-				if (func_call && func_call.name === functionName) {
-					var timeBlock = await provider.getBlock(curBlock);
-		  		var args = func_call.params.map(function (x) {return x.value});
-	  			function_calls.push({
-	  				from: txn.from,
-	  				args: args,
-						t: timeBlock.timestamp
-	  			})
-	  		}
-	  	}
-	  }
-	  curBlock = b.parentHash;
-	}
-	return function_calls;
+                // check that the function getting called in this txn is 'functionName'
+                if (func_call && func_call.name === functionName) {
+                    var timeBlock = await provider.getBlock(curBlock);
+                    var args = func_call.params.map(function (x) { return x.value });
+                    function_calls.push({
+                        from: txn.from,
+                        args: args,
+                        t: timeBlock.timestamp
+                    })
+                }
+            }
+        }
+        curBlock = b.parentHash;
+    }
+    return function_calls;
 }
 
 // We've provided a breadth-first search implementation for you, if that's useful
 // It will find a path from start to end (or return null if none exists)
 // You just need to pass in a function ('getNeighbors') that takes a node (string) and returns its neighbors (as an array)
 async function doBFS(start, end, getNeighbors) {
-	var queue = [[start]];
-	while (queue.length > 0) {
-		var cur = queue.shift();
-		var lastNode = cur[cur.length-1]
-		if (lastNode === end.toString()) {
-			return cur;
-		} else {
-			var neighbors = await getNeighbors(lastNode);
-			for (var i = 0; i < neighbors.length; i++) {
-				queue.push(cur.concat([neighbors[i]]));
-			}
-		}
-	}
-	return null;
+    var queue = [[start]];
+    while (queue.length > 0) {
+        var cur = queue.shift();
+        var lastNode = cur[cur.length - 1]
+        if (lastNode === end.toString()) {
+            return cur;
+        } else {
+            var neighbors = await getNeighbors(lastNode);
+            for (var i = 0; i < neighbors.length; i++) {
+                queue.push(cur.concat([neighbors[i]]));
+            }
+        }
+    }
+    return null;
 }
 
 // =============================================================================
@@ -355,59 +356,62 @@ async function doBFS(start, end, getNeighbors) {
 
 // This sets the default account on load and displays the total owed to that
 // account.
-provider.listAccounts().then((response)=> {
-	defaultAccount = response[0];
+provider.listAccounts().then((response) => {
+    defaultAccount = response[0];
 
-	getTotalOwed(defaultAccount).then((response)=>{
-		$("#total_owed").html("$"+response);
-	});
+    getTotalOwed(defaultAccount).then((response) => {
+        $("#total_owed").html("$" + response);
+    });
 
-	getLastActive(defaultAccount).then((response)=>{
-		time = timeConverter(response)
-		$("#last_active").html(time)
-	});
+    getLastActive(defaultAccount).then((response) => {
+        time = timeConverter(response)
+        $("#last_active").html(time)
+    });
 });
 
 // This code updates the 'My Account' UI with the results of your functions
-$("#myaccount").change(function() {
-	defaultAccount = $(this).val();
+$("#myaccount").change(function () {
+    defaultAccount = $(this).val();
 
-	getTotalOwed(defaultAccount).then((response)=>{
-		$("#total_owed").html("$"+response);
-	})
+    getTotalOwed(defaultAccount).then((response) => {
+        $("#total_owed").html("$" + response);
+    })
 
-	getLastActive(defaultAccount).then((response)=>{
-		time = timeConverter(response)
-		$("#last_active").html(time)
-	});
+    getLastActive(defaultAccount).then((response) => {
+        time = timeConverter(response)
+        $("#last_active").html(time)
+    });
 });
 
 // Allows switching between accounts in 'My Account' and the 'fast-copy' in 'Address of person you owe
-provider.listAccounts().then((response)=>{
-	var opts = response.map(function (a) { return '<option value="'+
-			a+'">'+a+'</option>' });
-	$(".account").html(opts);
-	$(".wallet_addresses").html(response.map(function (a) { return '<li>'+a+'</li>' }));
+provider.listAccounts().then((response) => {
+    var opts = response.map(function (a) {
+        return '<option value="' +
+            a + '">' + a + '</option>'
+    });
+    $(".account").html(opts);
+    $(".wallet_addresses").html(response.map(function (a) { return '<li>' + a + '</li>' }));
 });
 
 // This code updates the 'Users' list in the UI with the results of your function
-getUsers().then((response)=>{
-	$("#all_users").html(response.map(function (u,i) { return "<li>"+u+"</li>" }));
+getUsers().then((response) => {
+    $("#all_users").html(response.map(function (u, i) { return "<li>" + u + "</li>" }));
 });
 
 // This runs the 'add_IOU' function when you click the button
 // It passes the values from the two inputs above
-$("#addiou").click(function() {
-	defaultAccount = $("#myaccount").val(); //sets the default account
-  add_IOU($("#creditor").val(), $("#amount").val()).then((response)=>{
-		window.location.reload(false); // refreshes the page after add_IOU returns and the promise is unwrapped
-	})
+$("#addiou").click(function () {
+    defaultAccount = $("#myaccount").val(); //sets the default account
+    add_IOU($("#creditor").val(), $("#amount").val())
+        .then((response) => {
+            window.location.reload(false); // refreshes the page after add_IOU returns and the promise is unwrapped
+        })
 });
 
 // This is a log function, provided if you want to display things to the page instead of the JavaScript console
 // Pass in a discription of what you're printing, and then the object to print
 function log(description, obj) {
-	$("#log").html($("#log").html() + description + ": " + JSON.stringify(obj, null, 2) + "\n\n");
+    $("#log").html($("#log").html() + description + ": " + JSON.stringify(obj, null, 2) + "\n\n");
 }
 
 
@@ -423,50 +427,50 @@ function log(description, obj) {
 // async functions and thus will return a promise. Make sure you understand what this means.
 
 function check(name, condition) {
-	if (condition) {
-		console.log(name + ": SUCCESS");
-		return 3;
-	} else {
-		console.log(name + ": FAILED");
-		return 0;
-	}
+    if (condition) {
+        console.log(name + ": SUCCESS");
+        return 3;
+    } else {
+        console.log(name + ": FAILED");
+        return 0;
+    }
 }
 
 async function sanityCheck() {
-	console.log ("\nTEST", "Simplest possible test: only runs one add_IOU; uses all client functions: lookup, getTotalOwed, getUsers, getLastActive");
+    console.log("\nTEST", "Simplest possible test: only runs one add_IOU; uses all client functions: lookup, getTotalOwed, getUsers, getLastActive");
 
-	var score = 0;
+    var score = 0;
 
-	var accounts = await provider.listAccounts();
-	defaultAccount = accounts[0];
+    var accounts = await provider.listAccounts();
+    defaultAccount = accounts[0];
 
-	var users = await getUsers();
-	score += check("getUsers() initially empty", users.length === 0);
+    var users = await getUsers();
+    score += check("getUsers() initially empty", users.length === 0);
 
-	var owed = await getTotalOwed(accounts[1]);
-	score += check("getTotalOwed(0) initially empty", owed === 0);
+    var owed = await getTotalOwed(accounts[1]);
+    score += check("getTotalOwed(0) initially empty", owed === 0);
 
-	var lookup_0_1 = await BlockchainSplitwise.lookup(accounts[0], accounts[1]);
-	console.log("lookup(0, 1) current value" + lookup_0_1);
-	score += check("lookup(0,1) initially 0", parseInt(lookup_0_1, 10) === 0);
+    var lookup_0_1 = await BlockchainSplitwise.lookup(accounts[0], accounts[1]);
+    console.log("lookup(0, 1) current value" + lookup_0_1);
+    score += check("lookup(0,1) initially 0", parseInt(lookup_0_1, 10) === 0);
 
-	var response = await add_IOU(accounts[1], "10");
+    var response = await add_IOU(accounts[1], "10");
 
-	users = await getUsers();
-	score += check("getUsers() now length 2", users.length === 2);
+    users = await getUsers();
+    score += check("getUsers() now length 2", users.length === 2);
 
-	owed = await getTotalOwed(accounts[0]);
-	score += check("getTotalOwed(0) now 10", owed === 10);
+    owed = await getTotalOwed(accounts[0]);
+    score += check("getTotalOwed(0) now 10", owed === 10);
 
-	lookup_0_1 = await BlockchainSplitwise.lookup(accounts[0], accounts[1]);
-	score += check("lookup(0,1) now 10", parseInt(lookup_0_1, 10) === 10);
+    lookup_0_1 = await BlockchainSplitwise.lookup(accounts[0], accounts[1]);
+    score += check("lookup(0,1) now 10", parseInt(lookup_0_1, 10) === 10);
 
-	var timeLastActive = await getLastActive(accounts[0]);
-	var timeNow = Date.now()/1000;
-	var difference = timeNow - timeLastActive;
-	score += check("getLastActive(0) works", difference <= 60 && difference >= -3); // -3 to 60 seconds
+    var timeLastActive = await getLastActive(accounts[0]);
+    var timeNow = Date.now() / 1000;
+    var difference = timeNow - timeLastActive;
+    score += check("getLastActive(0) works", difference <= 60 && difference >= -3); // -3 to 60 seconds
 
-	console.log("Final Score: " + score +"/21");
+    console.log("Final Score: " + score + "/21");
 }
 
-// sanityCheck() //Uncomment this line to run the sanity check when you first open index.html
+//sanityCheck() //Uncomment this line to run the sanity check when you first open index.html
